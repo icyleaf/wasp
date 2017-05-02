2e7f6729b826fdf92d169528f2065952ac26a176
icyleaf <icyleaf.cn@gmail.com>
2017-05-02 18:09:18 +0800

    watch changes with wasp server

--- src/wasp/commands/server.cr
+++ src/wasp/commands/server.cr
@@ -10,14 +10,17 @@ class Wasp::Command
     class Options
       string %w(-b --baseURL), var: "string", default: "/", desc: "hostname (and path) to the root, e.g. http://icyleaf.com/"
       string %w(-p --port), var: "int", default: "8624", desc: "port on which the server will listen"
-      bool %w(-w --watch), default: true, desc: "watch filesystem for changes and recreate as needed"
+      bool %w(-w --watch), not: "-W", default: true, desc: "watch filesystem for changes and recreate as needed"
       help
     end
 
     def run
       # TODO: it does not works with localhost:8624 in HTTP::Server
       base_url = args.baseURL? ? args.baseURL : "/"
-      Build.run(["--source", args.source, "--baseURL", base_url])
+      build_args = ["--source", args.source, "--baseURL", base_url]
+      Build.run(build_args)
+
+      watching_changes(args.source, build_args) if args.watch?
 
       UI.message "Web Server is running at http://localhost:#{args.port} (Press Ctrl+C to stop)"
 
@@ -34,6 +37,18 @@ class Wasp::Command
       ])
 
       server.listen
+    end
+
+    def watching_changes(source, build_args)
+      watcher = Watcher.new(source)
+      spawn do
+        loop do
+          watcher.watch_changes do |file, status|
+            UI.verbose "#{file} was #{status}"
+            Build.run(build_args)
+          end
+        end
+      end
     end
   end
 end
---
+++ src/wasp/watcher.cr
@@ -0,0 +1,81 @@
+module Wasp
+  class Watcher
+    struct WatcherFile
+      property path, mtime, status
+
+      def initialize(@path : String, @mtime : Time)
+      end
+    end
+
+    enum WatchFileStatus
+      ADDED
+      CHANGED
+      DELETED
+    end
+
+    @source_path : String
+    @rules : Array(String)
+    @files : Hash(String, WatcherFile)
+
+    def initialize(source_path : String, @rules = ["config.yml", "contents/**/*.md", "layouts/**/*.html", "static/**/*"])
+      @source_path = File.expand_path(source_path)
+      @files = collect_files
+    end
+
+    def start
+      loop do
+        watch_changes do |file, status|
+          puts "#{file} was #{status}"
+        end
+        sleep 1
+      end
+    end
+
+    def watch_changes
+      @files.each do |file, watch|
+        if File.exists?(file)
+          latest_file_mtime = file_mtime(file)
+          if latest_file_mtime != watch.mtime
+            yield file, WatchFileStatus::CHANGED
+
+            watch.mtime = latest_file_mtime
+            @files[file] = watch
+          end
+        else
+          yield file, WatchFileStatus::DELETED
+        end
+      end
+
+      files = collect_files
+      if files.size > @files.size
+        new_files = files.keys - @files.keys
+        new_files.each do |file|
+          yield file, WatchFileStatus::ADDED
+        end
+      end
+      @files = files
+    end
+
+    def new_files
+      new_files = collect_files
+      if new_files.size > @files.size
+
+      end
+    end
+
+    private def collect_files
+      files = {} of String => WatcherFile
+      @rules.each do |path|
+        Dir.glob(File.join(@source_path, path)).each do |file|
+          files[file] = WatcherFile.new(file, file_mtime(file))
+        end
+      end
+
+      files
+    end
+
+    private def file_mtime(file)
+      File.stat(file).mtime
+    end
+  end
+end
