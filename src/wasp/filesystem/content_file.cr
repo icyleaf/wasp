5f07d1ac46a222726b958685619cf5e1e9d472b1
icyleaf <icyleaf.cn@gmail.com>
2017-05-04 22:52:34 +0800

    do not pass contents_path to ContentFile

--- src/wasp/filesystem/content_file.cr
+++ src/wasp/filesystem/content_file.cr
@@ -4,28 +4,38 @@ require "uri"
 
 module Wasp::FileSystem
   class ContentFile
-    getter contents_path, name, path, content
+    getter content
 
     @content : String
 
     FRONT_MATTER_REGEX = /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
 
-    def initialize(path : String, @site_config : Hash(YAML::Type, YAML::Type), contents_path : String)
-      @path = File.expand_path(path)
-      @contents_path = contents_path.ends_with?("/") ? contents_path : contents_path + "/"
-      @name = File.basename(@path)
+    def initialize(file : String, @site_config : Hash(YAML::Type, YAML::Type))
+      @file = File.expand_path(file)
+      @name = File.basename(@file)
 
-      text = File.read(@path)
+      text = File.read(@file)
       if text =~ FRONT_MATTER_REGEX
         @front_matter = FrontMatter.new($1)
       else
-        raise MissingFrontMatterError.new("Not fount metadata in " + path)
+        raise MissingFrontMatterError.new("Not found metadata in " + @file)
       end
 
       @content = Markdown.to_html(text.gsub(FRONT_MATTER_REGEX, ""))
     end
 
+    def section
+      sections = @file.split("/")
+      start_index = sections.index("contents").not_nil!
+      sections.delete_at(start_index + 1, (sections.size - start_index - 2)).join("/")
+    end
+
+    def filename
+      @name.chomp(File.extname(@name))
+    end
+
     def summary(limit = 300)
+      # TODO: process to pure text
       @content[0..limit] + " »"
     end
 
@@ -54,16 +64,16 @@ module Wasp::FileSystem
     def as_h
       @front_matter.as_h.merge({
         "summary" => summary,
-        "content" => content,
+        "content" => @content,
         "permalink" => permalink,
         "link" => link,
       })
     end
 
-    private def permalink_section(section)
-      return "" if !section || section.empty?
+    private def permalink_section(uri)
+      return "" if !uri || uri.empty?
 
-      case section
+      case uri
       when ":year"
         @front_matter.date.year
       when ":month"
@@ -73,10 +83,10 @@ module Wasp::FileSystem
       when ":title", ":slug"
         @front_matter.slug ? @front_matter.slug : URI.escape(@front_matter.title.downcase.gsub(" ", "-"))
       when ":section"
-        File.dirname(@path).gsub(@contents_path, "")
+        section
       else
         # such as :filename or others
-        @name.chomp(File.extname(@name))
+        filename
       end
     end
   end
